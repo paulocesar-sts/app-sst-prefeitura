17fbde38a221caf79cdf315cbd3ba5069b4d5baa
paulocesar-sts <paulocesar2025sts@gmail.com>
2026-09-18 11:56:55 -0300

    Update service worker cache version and asset list

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='app-sst-prefeitura-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request))));
+const CACHE='app-sst-prefeitura-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
